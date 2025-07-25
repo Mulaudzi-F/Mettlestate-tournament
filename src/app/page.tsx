15f1577729a1afee1c249ae3d2b5b99962ed7f30
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2025-07-25 20:23:55 +0200

    created a state that was shared with children component for opening a modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 // app/page.tsx
+"use client";
 import { Hero } from "@/components/sections/Hero";
 import { EventDetails } from "@/components/sections/EventDetails";
 
@@ -6,14 +7,19 @@ import { RegistrationForm } from "@/components/sections/RegistrationForm";
 import { FAQ } from "@/components/sections/FAQ";
 import { Footer } from "@/components/sections/Footer";
 import { Leaderboard } from "@/components/sections/LeaderBoard";
+import { useState } from "react";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <main>
-      <Hero />
+      <Hero setIsModalOpen={setIsModalOpen} />
       <EventDetails />
       <Leaderboard />
-      <RegistrationForm />
+      <RegistrationForm
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
       <FAQ />
       <Footer />
     </main>
